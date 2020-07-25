33c87fa4cb7dc955c95388755fe99244fb42440a
Andras Mocsary <nobody@reedwolf.com>
2020-07-25 19:16:10 +0200

    Fix photon_to_wasm.sh

--- photon_to_wasm.sh
+++ photon_to_wasm.sh
@@ -4,6 +4,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal -q -y
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 cd photon/crate
 git reset --hard
+echo -en '\n\n' >> Cargo.toml
 echo "[profile.release]" >> Cargo.toml
 echo 'lto = true' >> Cargo.toml
 echo "opt-level = 's'" >> Cargo.toml
@@ -16,4 +17,4 @@ rm photon_wasm_64.tsx
 echo -n 'const photon_wasm_64 = "' >> photon_wasm_64.tsx
 cat ../../../photon/crate/pkg/photon_rs_bg.wasm64 >> photon_wasm_64.tsx
 echo -n '"' >> photon_wasm_64.tsx
-echo "export default photon_wasm_64" >> photon_wasm_64.tsx
+echo -en '\n\nexport default photon_wasm_64' >> photon_wasm_64.tsx
